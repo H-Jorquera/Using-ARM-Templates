0989413140b5976a03c3ef5f99c657231295da9a
H-Jorquera <h.jorquera@mail.com>
2021-02-20 10:12:08 +1300

    Creation of Parametized ARM Template. Detailed further examples to deploy parametized ARM templates via PowerShell and Azure CLI

--- DeployARMTemplateToAzure/PwshAzModule.ps1
+++ DeployARMTemplateToAzure/PwshAzModule.ps1
@@ -8,9 +8,40 @@ New-AzResourceGroup `
   -Name {name of your resource group} `
   -Location "{location}"
 
-# 3. Deploy the template
+# 3. Deploy a template
 $templateFile = "{provide-the-path-to-the-template-file}"
 New-AzResourceGroupDeployment `
   -Name blanktemplate `
   -ResourceGroupName myResourceGroup `
   -TemplateFile $templateFile
+
+    # Example 1:
+        $templateFile = "azuredeploy.json"
+        $today=Get-Date -Format "MM-dd-yyyy"
+        $deploymentName="addstorage-"+"$today"
+        New-AzResourceGroupDeployment `
+        -Name $deploymentName `
+        -TemplateFile $templateFile
+
+    # Example 2: Deploy a parameterized ARM template. Provide a unique name and required storage SKU.  -storageSKU Standard_GRS
+    $templateFile = "parametized.Azuredeploy.json"
+    $today=Get-Date -Format "yyyy-MM-dd"
+    $deploymentName="addnameparameter-"+"$today"
+    New-AzResourceGroupDeployment `
+      -Name $deploymentName `
+      -TemplateFile $templateFile `
+      -storageName hjparametizedstorage2021
+      -storageName {your-unique-name} `
+      -storageSKU Standard_GRS
+
+# 4. Working with subscriptions
+Connect-AzAccount
+Get-AzSubscription
+
+    # Change your active subscription to the Desired Subscription
+        $context = Get-AzSubscription -SubscriptionId {Your subscription ID}
+        Set-AzContext $context
+
+    # If deploying to the same Resource group during entire session, set it as default. 
+    # This action lets you omit that parameter from the rest of the Azure PowerShell commands in this session
+        Set-AzDefault -ResourceGroupName azDeploy-RG
